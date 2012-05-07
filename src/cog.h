4bfb9c8913d63130dd6579a38b6969a8f75a74ee
Jonathan Frawley <jonathanfrawley@gmail.com>
2012-05-07 03:09:25 +0100

    Test for animation which hasn't been implemented yet.

--- src/cog.h
+++ src/cog.h
@@ -4,3 +4,9 @@ void cog_init();
 void cog_mainloop();
 void cog_destroy();
 
+//anim
+typedef struct cog_sprite
+{
+}cog_sprite;
+cog_sprite* cog_add_anim(char* animimg, int milliseconds, ...);
+void cog_play_anim(cog_sprite* sprite);
