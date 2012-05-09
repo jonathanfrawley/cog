55f53f3d339f572415561d60c2ff1aa58e5eee04
Jonathan Frawley <jonathanfrawley@gmail.com>
2012-05-09 08:03:43 +0100

    Simple file loading. Added cmake for opengl, GLES, basic fragment and vertex shaders.

--- src/cog.c
+++ src/cog.c
@@ -2,9 +2,14 @@
 #include <stdarg.h>
 #include <assert.h>
 #include <SDL/SDL.h>
+#include <GL/glew.h>
 
 //constants
-static COG_MAX_BUF = 255;
+static int COG_MAX_BUF = 255;
+static int COG_MAX_FILE_BUF = 4080;
+//TODO: Get these from config.
+static int COG_SCREEN_WIDTH = 640;
+static int COG_SCREEN_HEIGHT = 480;
 
 //data structures
 typedef struct
@@ -25,6 +30,9 @@ void cog_platform_init(void);
 void cog_window_init(void);
 void cog_window_togglefullscreen(void);
 void cog_checkkeys(void);
+void cog_graphics_init(void);
+GLuint cog_graphics_load_shader(char* filename, GLenum shadertype);
+void cog_read_file(char* buf, char* filename);
 
 //implementations
 void cog_init()
@@ -32,6 +40,7 @@ void cog_init()
     game.finished = 0;
     cog_platform_init();
     cog_window_init();
+    cog_graphics_init();
 }
 
 void cog_mainloop()
@@ -112,4 +121,48 @@ void cog_checkkeys(void)
                 }
         }
     }
+}
+
+//graphics
+void cog_graphics_init(void)
+{
+    glewInit();
+    if(!GLEW_VERSION_2_1)
+    {
+        //TODO: Revert to software rendering if not available.
+        perror("Error: OpenGL 2.1 not available, bailing out.");
+    }
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
+    glClearDepth(1.0f);
+    glEnable( GL_DEPTH_TEST );
+    glDepthFunc( GL_LEQUAL );
+    glViewport(0,0,COG_SCREEN_WIDTH,COG_SCREEN_HEIGHT);
+    if(! cog_graphics_init_shaders())
+    {
+        perror("Error creating shaders");
+    }
+}
+
+int cog_graphics_init_shaders(void)
+{
+    cog_graphics_load_shader("../src/simple2d.vert", GL_VERTEX_SHADER);
+    cog_graphics_load_shader("../src/simple2d.frag", GL_FRAGMENT_SHADER);
+}
+
+GLuint cog_graphics_load_shader(char* filename, GLenum shadertype)
+{
+    char filebuf[COG_MAX_FILE_BUF];
+    cog_read_file(filebuf, filename);
+    printf("file is : %s\n", filebuf);
+}
+
+void cog_read_file(char* buf, char* filename)
+{
+    FILE *fp;
+    fp=fopen(filename, "r");
+    fread(buf,
+           sizeof(char),
+           COG_MAX_BUF,
+           fp);
+    fclose(fp);
 }
