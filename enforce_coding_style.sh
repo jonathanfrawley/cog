5af862caf40064b817dd744cfacedc1a65ad4e34
Jonathan Frawley <jonathan@digitgaming.com>
2013-03-09 23:49:35 +0000

    Adding switch indent to this.

--- enforce_coding_style.sh
+++ enforce_coding_style.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . \( -name '*.c' -o -name '*.h' \) -exec printf "Indenting file {}\n" \; -exec astyle --style=java --unpad-paren --delete-empty-lines --add-brackets --convert-tabs --align-pointer=type --lineend=linux --suffix=none {} \;
+find . \( -name '*.c' -o -name '*.h' \) -exec printf "Indenting file {}\n" \; -exec astyle --style=java --suffix=.astyleorig --unpad-paren --delete-empty-lines --add-brackets --convert-tabs --align-pointer=type --lineend=linux --suffix=none --indent-switches {} \;
