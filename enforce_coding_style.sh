7a697cd535c50b404020647d1af6bf31b495316a
Jonathan Frawley <jonathan@digitgaming.com>
2013-03-09 13:37:07 +0000

    Modifying code style

--- enforce_coding_style.sh
+++ enforce_coding_style.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-for basedir in "src" "tests"
+for basedir in "src"
 do
     for file in `ls $basedir/*.{c,h}`
     do
         echo "##### Indenting file $file"
-        # I don't like the -cs option (space after casts)
-        # but indent doesn't recognise user types so we 
-        # should be consistent.
-        #indent -cs -br -ce -brf -i4 -nsai -nsaw -nsaf -npcs -nut -sob -npsl -nss $file -o $file
-        astyle --style=java --delete-empty-lines --add-brackets --convert-tabs --align-pointer=type --lineend=linux --suffix=none $file
+        astyle --style=java --unpad-paren --delete-empty-lines --add-brackets --convert-tabs --align-pointer=type --lineend=linux --suffix=none $file
     done
 done
