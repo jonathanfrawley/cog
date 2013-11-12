b919346cef3642f4619eff55fb1cb6b87db219b9
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-11-12 22:14:26 +0000

    Working on windows :D

--- build_in_place.sh
+++ build_in_place.sh
@@ -1,6 +1,6 @@
 
 #export LD_LIBRARY_PATH=win32/lib && make -f Makefile.static clean && make -f Makefile.static
-dest_dir=win32
+dest_dir=../win32
 
 export CMAKE_LIBRARY_PATH="${CMAKE_LIBRARY_PATH}:$dest_dir/lib"
 export CMAKE_INCLUDE_PATH="${CMAKE_INCLUDE_PATH}:$dest_dir/include/AL:$dest_dir/include/SDL2:$dest_dir/include/freetype2:$dest_dir/include/"
