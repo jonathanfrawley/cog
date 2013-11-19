9c4d1018f8d9c0d3badc8ee11d60e22bb146cf5c
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-11-19 00:05:13 +0000

    Fun test

--- lang/python/test_anim.py
+++ lang/python/test_anim.py
@@ -1,3 +1,5 @@
+import random
+
 import cog
 
 cog.init()
@@ -5,21 +7,21 @@ cog.init()
 x_dim = 0.1
 y_dim = 0.1
 kittens = []
-for i in range(0, 10):
+for i in range(0, 200):
     kitten_anim_id = cog.anim_add("media/kitten_anim.png", 3)
     cog.anim_set_frames(kitten_anim_id, 0, 1, 2)
     kitten = cog.anim_get(kitten_anim_id)
     kitten.dim = cog.dim2()
-    kitten.pos.x = x_dim * i - 1.0
-    kitten.dim.h = y_dim * i - 1.0
+    kitten.pos.x = random.random() * 2.0 - 1
+    kitten.dim.y = random.random() * 2.0 - 1
     kitten.dim.w = x_dim
     kitten.dim.h = y_dim
     kitten.transition_millis = 150
     kitten.looped = True
-    kitten.ang_vel = cog.COG_PI / 14000
+    kitten.ang_vel = cog.COG_PI / (14000 * random.random())
     kitten.vel = cog.vec2()
-    kitten.vel.x = 0.0001
-    kitten.vel.y = 0.0003
+    kitten.vel.x = random.random() * 0.001 * random.choice([-1, 1])
+    kitten.vel.y = random.random() * 0.003 * random.choice([-1, 1])
     kittens.append(kitten_anim_id)
 
 while not cog.hasquit():
