5a89e9711875073f9dd7ae4e57f333d4f5ca5504
Jonathan Frawley <jmf@9lines.org>
2014-02-02 23:43:32 +0000

    Adding an example for tiled test

--- tests/test_tiled.c
+++ tests/test_tiled.c
@@ -1,40 +1,150 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <xml.h>
+/**
+ * section: xmlReader
+ * synopsis: Parse an XML file with an xmlReader
+ * purpose: Demonstrate the use of xmlReaderForFile() to parse an XML file
+ *          and dump the informations about the nodes found in the process.
+ *          (Note that the XMLReader functions require libxml2 version later
+ *          than 2.6.)
+ * usage: reader1 <filename>
+ * test: reader1 test2.xml > reader1.tmp && diff reader1.tmp $(srcdir)/reader1.res
+ * author: Daniel Veillard
+ * copy: see Copyright for the status of this software.
+ */
 
+#include <stdio.h>
+#include <libxml2/libxml/xmlreader.h>
+
+#ifdef LIBXML_READER_ENABLED
 
 /**
- * * Will halt the program iff assertion fails
- * */
-static void _assert_that(_Bool condition, char const* message, char const* func, char const* file, int line) {
-    if (!condition) {
-        fprintf(stderr, "Assertion failed: %s, in %s (%s:%i)\n", message, func, file, line);
-        exit(EXIT_FAILURE);
+ * processNode:
+ * @reader: the xmlReader
+ *
+ * Dump information about the current node
+ */
+static void processNode(xmlTextReaderPtr reader) {
+    const xmlChar *name, *value;
+
+    name = xmlTextReaderConstName(reader);
+    if (name == NULL)
+    name = BAD_CAST "--";
+
+    //printf("NAME : <%s> \n", name);
+    if(xmlStrEqual("map", name)) {
+        printf("Parsing map...\n");
+        int attrib_cnt = xmlTextReaderAttributeCount(reader);
+        //printf("Attrib cnt %d ", attrib_cnt);
+        printf("width is <%s>\n", xmlTextReaderGetAttribute(reader, "width"));
+        printf("height is <%s>\n", xmlTextReaderGetAttribute(reader, "height"));
+        printf("tilewidth is <%s>\n", xmlTextReaderGetAttribute(reader, "tilewidth"));
+        printf("tileheight is <%s>\n", xmlTextReaderGetAttribute(reader, "tileheight"));
+    } else if(xmlStrEqual("tileset", name)) {
+        printf("Parsing tileset...\n");
+        printf("firstgid is <%s>\n", xmlTextReaderGetAttribute(reader, "firstgid"));
+        printf("name is <%s>\n", xmlTextReaderGetAttribute(reader, "name"));
+        printf("tilewidth is <%s>\n", xmlTextReaderGetAttribute(reader, "tilewidth"));
+        printf("tileheight is <%s>\n", xmlTextReaderGetAttribute(reader, "tileheight"));
+    } else if(xmlStrEqual("image", name)) {
+        printf("Parsing image...\n");
+        printf("source is <%s>\n", xmlTextReaderGetAttribute(reader, "source"));
+        printf("width is <%s>\n", xmlTextReaderGetAttribute(reader, "width"));
+        printf("height is <%s>\n", xmlTextReaderGetAttribute(reader, "height"));
+    } else if(xmlStrEqual("layer", name)) {
+        printf("Parsing layer...\n");
+        printf("name is <%s>\n", xmlTextReaderGetAttribute(reader, "name"));
+        printf("width is <%s>\n", xmlTextReaderGetAttribute(reader, "width"));
+        printf("height is <%s>\n", xmlTextReaderGetAttribute(reader, "height"));
+    } else if(xmlStrEqual("data", name)) {
+        printf("Parsing data...\n");
+        printf("data is <%s>\n", xmlTextReaderConstValue(reader));
+        //TODO: All children
+    } else if(xmlStrEqual("objectgroup", name)) {
+        printf("Parsing objectgroup...\n");
+        printf("name is <%s>\n", xmlTextReaderGetAttribute(reader, "name"));
+        printf("width is <%s>\n", xmlTextReaderGetAttribute(reader, "width"));
+        printf("height is <%s>\n", xmlTextReaderGetAttribute(reader, "height"));
+    } else if(xmlStrEqual("object", name)) {
+        printf("Parsing object...\n");
+        printf("name is <%s>\n", xmlTextReaderGetAttribute(reader, "name"));
+        printf("x is <%s>\n", xmlTextReaderGetAttribute(reader, "x"));
+        printf("y is <%s>\n", xmlTextReaderGetAttribute(reader, "y"));
+        printf("width is <%s>\n", xmlTextReaderGetAttribute(reader, "width"));
+        printf("height is <%s>\n", xmlTextReaderGetAttribute(reader, "height"));
+    }
+
+    //value = xmlTextReaderConstValue(reader);
+/*
+    printf("%d %d %s %d %d", 
+        xmlTextReaderDepth(reader),
+        xmlTextReaderNodeType(reader),
+        name,
+        xmlTextReaderIsEmptyElement(reader),
+        xmlTextReaderHasValue(reader));
+    if(value == NULL) {
+        printf("\n");
+    } else {
+        if (xmlStrlen(value) > 40)
+            printf(" %.40s...\n", value);
+        else
+        printf(" %s\n", value);
+    }
+*/
+}
+
+/**
+ * streamFile:
+ * @filename: the file name to parse
+ *
+ * Parse and print information about an XML file.
+ */
+static void streamFile(const char *filename) {
+    xmlTextReaderPtr reader;
+    int ret;
+
+    reader = xmlReaderForFile(filename, NULL, 0);
+    if (reader != NULL) {
+        ret = xmlTextReaderRead(reader);
+        while (ret == 1) {
+            processNode(reader);
+            ret = xmlTextReaderRead(reader);
+        }
+        xmlFreeTextReader(reader);
+        if (ret != 0) {
+            fprintf(stderr, "%s : failed to parse\n", filename);
+        }
+    } else {
+        fprintf(stderr, "Unable to open %s\n", filename);
     }
 }
 
-#define assert_that(condition, message) \
-    _assert_that(condition, message, __func__, __FILE__, __LINE__)
+int main(int argc, char **argv) {
+    if (argc != 2)
+        return(1);
 
+    /*
+     * this initialize the library and check potential ABI mismatches
+     * between the version it was compiled for and the actual shared
+     * library used.
+     */
+    LIBXML_TEST_VERSION
 
+    streamFile(argv[1]);
 
-int main(int argc, char** argv) {
-#define FILE_NAME "test.xml"
-    FILE* handle = fopen(FILE_NAME, "rb");
-    assert_that(handle, "Cannot open " FILE_NAME);
-
-    struct xml_document* document = xml_open_document(handle);
-    assert_that(document, "Cannot parse " FILE_NAME);
-
-    struct xml_node* element = xml_easy_child(
-            xml_document_root(document), "Element", "With", 0
-            );
-    assert_that(element, "Cannot find Document/Element/With");
-
-
-    fprintf(stderr, "HI! Element content is : %s", xml_easy_content(element));
-
-    xml_document_free(document, true);
-#undef FILE_NAME
+    /*
+     * Cleanup function for the XML library.
+     */
+    xmlCleanupParser();
+    /*
+     * this is to debug memory for regression tests
+     */
+    xmlMemoryDump();
+    return(0);
 }
+
+#else
+int main(void) {
+    fprintf(stderr, "XInclude support not compiled in\n");
+    exit(1);
+}
+#endif
+
