8cdfb09c4b03a7aa85c06e6667b18603a02fca33
Jonathan Frawley <jmf@9lines.org>
2014-03-12 00:28:19 +0000

    Linear interpolation for images

--- src/cog_graphics_opengl.c
+++ src/cog_graphics_opengl.c
@@ -322,8 +322,9 @@ GLuint cog_graphics_opengl_load_texture_png(const char* file_name, int* width, i
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexImage2D(GL_TEXTURE_2D, 0, format, temp_width, temp_height, 0, format, GL_UNSIGNED_BYTE, image_data);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
     // clean up
     png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
     free(image_data);
