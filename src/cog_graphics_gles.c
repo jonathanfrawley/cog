c38097141e26119dec82ff1c86da343dd177dacb
Jonathan Frawley <jmf@9lines.org>
2014-04-21 18:25:16 +0100

    Adding texture rendering to gles code

--- src/cog_graphics_gles.c
+++ src/cog_graphics_gles.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <time.h> //TODO:Remove
 
+#include <png.h>
+
 #include "gles_util/esUtil.h"
 
 #include "cog_log.h"
@@ -14,35 +16,8 @@
 GLuint program_object;
 GLuint index_object;
 
-//-------------Private functions
-static GLuint _load_shader(GLenum type, const char* shader_src) {
-    GLuint shader;
-    GLint compiled;
-    // Create the shader object
-    shader = glCreateShader(type);
-    if(shader == 0) {
-        return 0;
-    }
-    // Load the shader source
-    glShaderSource(shader, 1, &shader_src, NULL);
-    // Compile the shader
-    glCompileShader(shader);
-    // Check the compile status
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-    if(!compiled) {
-        GLint info_len = 0;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_len);
-        if(info_len > 1) {
-            char* info_log = malloc(sizeof(char) * info_len);
-            glGetShaderInfoLog(shader, info_len, NULL, info_log);
-            esLogMessage("Error compiling shader:\n%s\n", info_log);
-            free(info_log);
-        }
-        glDeleteShader(shader);
-        return 0;
-    }
-    return shader;
-}
+static GLuint _load_shader(GLenum type, const char* shader_src);
+static GLuint cog_graphics_gles_load_texture_png(const char* file_name, int* width, int* height);
 
 //-------------Public functions
 void cog_graphics_gles_draw_sprite(cog_sprite* sprite, uint32_t idx) {
@@ -84,8 +59,7 @@ void cog_graphics_gles_draw_sprite(cog_sprite* sprite, uint32_t idx) {
                             0.5, -0.5, 0.0,  // Position 2
                             0.5,  0.5, 0.0,  // Position 3
     };
-    GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
-
+    GLushort indices[] = {0, 1, 2, 0, 2, 3};
 
     // No clientside arrays, so do this in a webgl-friendly manner
     // vertex pos
@@ -164,17 +138,179 @@ void cog_graphics_gles_draw_text(cog_text* text) {
 void cog_graphics_gles_flush() {
 }
 
-uint32_t cog_graphics_gles_load_texture(const char* filename, int* width, int* height) {
-}
-
 void cog_graphics_gles_clear() {
 }
 
 void cog_graphics_gles_draw() {
 }
 
+uint32_t cog_graphics_gles_load_texture(const char* filename, int* width, int* height) {
+    return cog_graphics_gles_load_texture_png(filename, 0, 0); //TODO: Don't duplicate
+}
+
 void cog_graphics_gles_prepare(uint32_t amount) {
 }
 
 void cog_graphics_gles_set_camera_pos(cog_pos2* pos) {
+}
+
+//-------------Private functions
+static GLuint _load_shader(GLenum type, const char* shader_src) {
+    GLuint shader;
+    GLint compiled;
+    // Create the shader object
+    shader = glCreateShader(type);
+    if(shader == 0) {
+        return 0;
+    }
+    // Load the shader source
+    glShaderSource(shader, 1, &shader_src, NULL);
+    // Compile the shader
+    glCompileShader(shader);
+    // Check the compile status
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if(!compiled) {
+        GLint info_len = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_len);
+        if(info_len > 1) {
+            char* info_log = malloc(sizeof(char) * info_len);
+            glGetShaderInfoLog(shader, info_len, NULL, info_log);
+            esLogMessage("Error compiling shader:\n%s\n", info_log);
+            free(info_log);
+        }
+        glDeleteShader(shader);
+        return 0;
+    }
+    return shader;
+}
+
+//TODO:Don't duplicate this in 2 places
+/*-----------------------------------------------------------------------------
+ * Loads a transparent png image as a texure and uploads it to the GPU.
+ * Returns: A reference to the texture which can be passed to glBindTexture
+ *
+ * Notes:
+ * Source: https://github.com/DavidEGrayson/ahrs-visualizer/blob/master/png_texture.cpp
+ * License: https://github.com/DavidEGrayson/ahrs-visualizer/blob/master/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+static GLuint cog_graphics_gles_load_texture_png(const char* file_name, int* width, int* height) {
+    png_byte header[8];
+    FILE* fp = fopen(file_name, "rb");
+    if(fp == 0) {
+        perror(file_name);
+        return 0;
+    }
+    // read the header
+    fread(header, 1, 8, fp);
+    if(png_sig_cmp(header, 0, 8)) {
+        cog_errorf("error: %s is not a PNG.\n", file_name);
+        fclose(fp);
+        return 0;
+    }
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if(!png_ptr) {
+        cog_errorf("error: png_create_read_struct returned 0.\n");
+        fclose(fp);
+        return 0;
+    }
+    // create png info struct
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if(!info_ptr) {
+        cog_errorf("error: png_create_info_struct returned 0.\n");
+        png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+        fclose(fp);
+        return 0;
+    }
+    // create png info struct
+    png_infop end_info = png_create_info_struct(png_ptr);
+    if(!end_info) {
+        cog_errorf("error: png_create_info_struct returned 0.\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
+        fclose(fp);
+        return 0;
+    }
+    // the code in this if statement gets called if libpng encounters an error
+    if(setjmp(png_jmpbuf(png_ptr))) {
+        cog_errorf("error from libpng\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        fclose(fp);
+        return 0;
+    }
+    // init png reading
+    png_init_io(png_ptr, fp);
+    // let libpng know you already read the first 8 bytes
+    png_set_sig_bytes(png_ptr, 8);
+    // read all the info up to the image data
+    png_read_info(png_ptr, info_ptr);
+    // variables to pass to get info
+    int bit_depth, color_type;
+    png_uint_32 temp_width, temp_height;
+    // get info about png
+    png_get_IHDR(png_ptr, info_ptr, &temp_width, &temp_height, &bit_depth, &color_type,
+                 NULL, NULL, NULL);
+    if(width) {
+        *width = temp_width;
+    }
+    if(height) {
+        *height = temp_height;
+    }
+    //printf("%s: %lux%lu %d\n", file_name, temp_width, temp_height, color_type);
+    if(bit_depth != 8) {
+        cog_errorf("%s: Unsupported bit depth %d. Must be 8.\n", file_name, bit_depth);
+        return 0;
+    }
+    GLint format;
+    switch(color_type) {
+        case PNG_COLOR_TYPE_RGB:
+            format = GL_RGB;
+            break;
+        case PNG_COLOR_TYPE_RGB_ALPHA:
+            format = GL_RGBA;
+            break;
+        default:
+            cog_errorf("%s: Unknown libpng color type %d.\n", file_name, color_type);
+            return 0;
+    }
+    // Update the png info struct.
+    png_read_update_info(png_ptr, info_ptr);
+    // Row size in bytes.
+    int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+    // glTexImage2d requires rows to be 4-byte aligned
+    rowbytes += 3 - ((rowbytes-1) % 4);
+    // Allocate the image_data as a big block, to be given to opengl
+    png_byte* image_data = (png_byte*)malloc(rowbytes * temp_height * sizeof(png_byte)+15);
+    if(image_data == NULL) {
+        cog_errorf("error: could not allocate memory for PNG image data\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        fclose(fp);
+        return 0;
+    }
+    // row_pointers is for pointing to image_data for reading the png with libpng
+    png_byte** row_pointers = (png_byte**)malloc(temp_height * sizeof(png_byte*));
+    if(row_pointers == NULL) {
+        cog_errorf("error: could not allocate memory for PNG row pointers\n");
+        png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+        free(image_data);
+        fclose(fp);
+        return 0;
+    }
+    // set the individual row_pointers to point at the correct offsets of image_data
+    for(unsigned int i = 0; i < temp_height; i++) {
+        row_pointers[temp_height - 1 - i] = image_data + i * rowbytes;
+    }
+    // read the png into image_data through row_pointers
+    png_read_image(png_ptr, row_pointers);
+    // Generate the OpenGL texture object
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, temp_width, temp_height, 0, format, GL_UNSIGNED_BYTE, image_data);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    // clean up
+    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+    free(image_data);
+    free(row_pointers);
+    fclose(fp);
+    return texture;
 }
