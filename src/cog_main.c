87be2655143ab56f535448665eb4aa0bec4d3c77
Jonathan Frawley <jmf@9lines.org>
2015-11-22 15:35:43 +0000

    Debug config option

--- src/cog_main.c
+++ src/cog_main.c
@@ -51,6 +51,7 @@ double cog_math_radtodeg(double rad);
 //#global vars
 static cog_window window;
 //##timing
+static bool debug;
 static uint32_t delta_millis;
 static uint32_t starttime;
 static uint32_t now;
@@ -75,6 +76,7 @@ void _cog_init(cog_config config) {
     cog_tween_init();
     cog_rect_init();
     starttime = SDL_GetTicks();
+		debug = config.debug;
     //init rng
     srand(2);
 }
@@ -126,19 +128,19 @@ void cog_update() {
     }
     cog_input_check_keys();
     cog_input_check_mouse();
+if(debug) {
     //Useful logging every second.
     frametimecounter += lastframetime;
     if(frametimecounter >= 1000) {
-#ifdef DEBUG
         cog_debugf("nupdates <%d>, ndraws <%d>, render_time <%d> cog_map_get_counter <%d> cog_map_get_timer <%lf>", frameupdatecounter,
                    framedrawcounter, render_time, cog_map_get_counter(), cog_map_get_timer());
-#endif //DEBUG
         frametimecounter = 0;
         framedrawcounter = 0;
         frameupdatecounter = 0;
         cog_map_reset_counter();
         cog_map_reset_timer();
     }
+}
 }
 
 //This is to allow the user to control the mainloop
